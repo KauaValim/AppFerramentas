11028d95d77ced3d3cf87deed852bed1abd3dd8a
vieiradaniels <135739053+vieiradaniels@users.noreply.github.com>
2024-04-17 19:32:11 -0300

    aula17-04parte1

--- src/controller/OVController.java
+++ src/controller/OVController.java
@@ -9,33 +9,16 @@ public class OVController {
     private OVDAO ovDAO;
 
     public OVController() {
-        ovDAO = new OVDAO(); 
+        ovDAO = new OVDAO();
     }
 
-    public boolean adicionarOV(OrdemVenda ov) { 
-        if (ovDAO.adicionarOV(ov)) {
+    public boolean adicionarOV(OrdemVenda ov) {
+        if (ovDAO.adicionarOrdemVenda(ov)) {
             return true;
         } else {
-            JOptionPane.showMessageDialog(null, "Ordem de venda não cadastrada"); 
+            JOptionPane.showMessageDialog(null, "Ordem de venda não cadastrada");
             return false;
         }
     }
 
-    public boolean alterarOV(OrdemVenda ov) { 
-        if (ovDAO.alterarOV(ov)) { 
-            return true;
-        } else {
-            JOptionPane.showMessageDialog(null, "Ordem de venda não alterada"); 
-            return false;
-        }
-    }
-
-    public boolean excluirOV(Long numOrdemVenda) { 
-        if (ovDAO.excluirOV(numOrdemVenda)) {
-            return true;
-        } else {
-            JOptionPane.showMessageDialog(null, "Ordem de venda não excluída"); 
-            return false;
-        }
-    }
 }
